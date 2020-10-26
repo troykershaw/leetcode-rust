3826c751244a2623704d68023214187a84e9c68d
Troy Kershaw <hello@troykershaw.com>
2020-10-25 21:23:30 -0400

    Refactored two sum to be clearer

--- tests/p0001_two_sum.rs
+++ tests/p0001_two_sum.rs
@@ -16,22 +16,46 @@ return [0, 1].
 
 use std::collections::HashMap;
 
-fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    if nums.len() < 2 { panic!("Leetcode, don't give me vectors that are too small...") };
+fn two_sum_naive(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    if nums.len() < 2 { panic!("'nums' must have at least two items.") };
 
-    let mut diffs: HashMap<i32, usize> = HashMap::new();
-
-    for (i, n) in nums.iter().enumerate() {
-        match diffs.get(&n) {
-            Some(&i0) => return vec![i0 as i32, i as i32],
-            None => { diffs.insert(&target - n, i); }
+    for i in 0..nums.len()-1 {
+        for j in i+1..nums.len() {
+            if nums[i] + nums[j] == target {
+                return vec![i as i32, j as i32]
+            }
         }
     }
+
+    panic!("No items sum to the target")
+}
+
+#[test]
+fn two_sum_naive_test() {
+    assert_eq!(two_sum_naive(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(two_sum_naive(vec![2, 5, 3], 8), vec![1, 2]);
+    assert_eq!(two_sum_naive(vec![2, 9, 4, 3, -6], -2), vec![2, 4]);
+}
+
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    if nums.len() < 2 { panic!("'nums' must have at least two items.") };
+
+    let mut prev: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
+
+    for (i, n) in nums.iter().enumerate() {
+        if let Some(p) = prev.get(&(target - n)) {
+            return vec![*p as i32, i as i32]
+        }
+
+        prev.insert(*n, i);
+    }
     
-    panic!("Leetcode said this could never happen")
+    panic!("No items sum to the target")
 }
 
 #[test]
 fn two_sum_test() {
     assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(two_sum(vec![2, 5, 3], 8), vec![1, 2]);
+    assert_eq!(two_sum(vec![2, 9, 4, 3, -6], -2), vec![2, 4]);
 }
